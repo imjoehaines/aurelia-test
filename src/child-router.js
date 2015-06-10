a8be2834ea0456abb3aab231ccd7cfc4c5b88565
Joe Haines <hello@joehaines.co.uk>
2015-06-10 20:01:10 +0100

    Following aurelia tutorial

--- src/child-router.js
+++ src/child-router.js
@@ -3,11 +3,12 @@ export class ChildRouter{
 
   configureRouter(config, router){
     config.map([
-      { route: ['','welcome'],  moduleId: './welcome',      nav: true, title:'Welcome' },
-      { route: 'flickr',        moduleId: './flickr',       nav: true, title:'Flickr' },
-      { route: 'child-router',  moduleId: './child-router', nav: true, title:'Child Router' }
+      { route: ['','welcome'], name: 'welcome',     moduleId: './welcome',      nav: true, title:'Welcome' },
+      { route: 'flickr',       name: 'flickr',      moduleId: './flickr',       nav: true, title:'flickr' },
+      { route: 'child-router', name: 'childRouter', moduleId: './child-router', nav: true, title:'Child Router' }
     ]);
 
     this.router = router;
   }
 }
+
